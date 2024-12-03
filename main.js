b809d6f6e52f06ab87801d9a07537bb398339acf
Julia Marie Abad Bacud <144574620+waactics@users.noreply.github.com>
2024-12-03 11:38:27 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,26 +1,40 @@
-// BASE PET STATS, range 1-5
-const hunger = 5;
-const energy = 5;
-const happiness = 5;
+// RANDMOMIZE BASE PET STATS
+function getRandomStat() {
+  return Math.floor(Math.random() * 5) + 1;
+}
+
+let pet = {
+  hunger: getRandomStat(),
+  energy: getRandomStat(),
+  happiness: getRandomStat()
+};
+
+console.log(pet);
 
 
-// OUTPUT: INTRODUCE PET & CURRENT STATS
+// RANDOMIZE END GOAL
+// if hunger -> feed until stat reaches 5
+// if energy -> sleep until stat reaches 5
+// if happiness -> play until stat reaches 5
 
 
 // INPUT: ASK A NAME FOR THE PET
 
 
+// OUTPUT: INTRODUCE PET, CURRENT STATS, AND END GOAL
+
+
+// LOOP BEGINS HERE//
+
 // INPUT: PROMPT USER FOR ACTION
-// feed, play, or rest
-// if user enters "QUIT", terminate the program
+// feed, play, or sleep
+// if user enters "QUIT", terminate loop
 // if user enters invalid input, request another input
 
-
 // VERIFY STATS ARE AT MINIMUM REQUIREMENT
-// if hunger is too low -> cannot rest
+// if hunger is too low -> cannot sleep
 // if energy is too low -> cannot play
-// if happiness is too low -> cannot feed
-
+// if happiness is too low -> cannot eat
 
 // OUTPUT: DISPLAY STAT RESULTS
 // if stat reqs are fulfilled -> continue w calculations
@@ -29,11 +43,13 @@ const happiness = 5;
   // rest -> decrease happiness, increase hunger
 // else -> block action and output another prompt
 
-
 // DISPLAY CONSEQUENCES OF ACTION
-
-
-// LOOP TO ASK FOR PROMPT AGAIN
   // if any base stats reach 0 or below, output print that pet is taken away bc you are a bad owner
   // if user enters "QUIT", terminate the program
+  // if base stats are fine AND they type anything other than "QUIT", continue with loop
+
+// LOOP ENDS HERE //
+
+
+// DISPLAY "TY FOR PLAYING" MESSAGE 
 
